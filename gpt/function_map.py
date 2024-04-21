638886d4f214a2885779cc2831296087df8d6705
yono.ji <18262905661@163.com>
2024-04-21 19:57:46 +0800

    Support Order by LLM

--- gpt/function_map.py
+++ gpt/function_map.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
-def callback_support(callback_info):
+def callback_support(callback_info, request_body):
     if callback_info:
         url = callback_info.get('url')
         body = callback_info.get('body')
@@ -8,11 +8,17 @@ def callback_support(callback_info):
 
         if url:
             print(f"{url},{body}, {headers}")
-            response = requests.post(url, json=body, headers=headers)
-            return jsonify(response.json()), response.status_code
+            timeout = (5, 10)
+            body.update(request_body)
+            print(body)
+            response = requests.post(url, json=body, headers=headers, timeout=timeout)
+            print(response.text)
+            return response.text
 
 
 def sing_voice(sing, user_id, callback_info):
-    callback_support(callback_info)
-    print(f"{user_id} order {sing}")
-    return "已经在你的频道做播放"
+    request_body = {
+        "user_id": user_id,
+        "sing_name": sing
+    }
+    return callback_support(callback_info, request_body)
